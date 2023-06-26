b0ae1eb5126943e311073f984fc5b436f7d38e7d
JulienM <julienm@marmelab.com>
2023-06-26 11:54:21 +0200

    add greenframe

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <img alt="Marmelab logo" src="./assets/logo-moutarde.png" />
     <TitleMessage msg="Gildas, on arrive, met la moutarde au frais !!!" />
     <CountDown deadline="2023-06-28T15:20:00.000Z" />
+
+    <div id="greenframe_embedable_score_page"></div>
   </div>
 </template>
 
@@ -19,7 +21,10 @@ export default {
   },
 };
 </script>
-
+<script
+  id="greenframe_embedable_score_page_script"
+  src="https://cc7b-81-250-164-94.ngrok-free.app/embedable_score_page.js"
+></script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
